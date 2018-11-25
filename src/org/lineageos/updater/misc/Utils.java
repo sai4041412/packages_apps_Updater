55ce347ab5369ff54bdc173bf20cdf197a343033
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-11-25 23:12:31 +0700

    updater: Refracter for Xtended
    Change-Id: I0697c3eb6338a641d610c48225152f62865f7109
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- src/org/lineageos/updater/misc/Utils.java
+++ src/org/lineageos/updater/misc/Utils.java
@@ -151,7 +151,6 @@ public class Utils {
     }
 
     public static String getServerURL(Context context) {
-        String incrementalVersion = SystemProperties.get(Constants.PROP_BUILD_VERSION_INCREMENTAL);
         String device = SystemProperties.get(Constants.PROP_NEXT_DEVICE,
                 SystemProperties.get(Constants.PROP_DEVICE));
         String type = SystemProperties.get(Constants.PROP_RELEASE_TYPE).toLowerCase(Locale.ROOT);
@@ -161,9 +160,7 @@ public class Utils {
             serverUrl = context.getString(R.string.updater_server_url);
         }
 
-        return serverUrl.replace("{device}", device)
-                .replace("{type}", type)
-                .replace("{incr}", incrementalVersion);
+        return serverUrl.replace("{device}", device);
     }
 
     public static String getUpgradeBlockedURL(Context context) {
