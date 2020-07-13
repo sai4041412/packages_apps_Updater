10a262d3226f45abbcabf451067761dce59e1407
ashwatthama <sai404142@gmail.com>
2020-07-13 18:20:21 +0000

    Refactor to Xtended
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- push-update.sh
+++ push-update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-updates_dir=/data/colt_updates
+updates_dir=/data/xtended_updates
 
 if [ ! -f "$1" ]; then
    echo "Usage: $0 ZIP [UNVERIFIED]"
    echo "Push ZIP to $updates_dir and add it to Updater"
    echo
-   echo "The name of ZIP is assumed to have ColtOS-VERSION-DATE-TYPE-* as format"
+   echo "The name of ZIP is assumed to have Xtended-XQ-VERSION-DATE-TYPE-* as format"
    echo "If UNVERIFIED is set, the app will verify the update"
    exit
 fi
@@ -33,7 +33,7 @@ else
     status=2
 fi
 
-# Assume ColtOS-VERSION-DATE-TYPE-*.zip
+# Assume Xtended-XQ-VERSION-DATE-TYPE-*.zip
 zip_name=`basename "$zip_path"`
 id=`echo "$zip_name" | sha1sum | cut -d' ' -f1`
 version=`echo "$zip_name" | cut -d'-' -f2`
@@ -47,8 +47,8 @@ adb shell chgrp cache "$zip_path_device"
 adb shell chmod 664 "$zip_path_device"
 
 # Kill the app before updating the database
-adb shell "killall com.colt.updater 2>/dev/null"
-adb shell "sqlite3 /data/data/com.colt.updater/databases/updates.db" \
+adb shell "killall com.xtended.updater 2>/dev/null"
+adb shell "sqlite3 /data/data/com.xtended.updater/databases/updates.db" \
     "\"INSERT INTO updates (status, path, download_id, timestamp, type, version, size)" \
     "  VALUES ($status, '$zip_path_device', '$id', $timestamp, '$type', '$version', $size)\""
 
